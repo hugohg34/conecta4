6949e6ecd5c904727e389819924a19605ffcd97a
victor <hugohg34@gmail.com>
2021-10-12 18:48:42 +0200

    Board ajustado

--- src/main/java/view/DiscDropView.java
+++ src/main/java/view/DiscDropView.java
@@ -7,7 +7,7 @@ import utils.Console;
 public class DiscDropView {
 
 	public DiscDrop dropping(DiscColor color) {
-		int column = Console.getInstance().readInt("Please insert column to dropping disc of color " + color.toString());
+		int column = Console.getInstance().readInt("Insert column number to dropping disc of color " + color.toString());
 		return new DiscDrop(color, column);
 		
 	}
