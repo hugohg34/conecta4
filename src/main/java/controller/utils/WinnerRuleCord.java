cd2f8fe478bfac82f3ee118eb60bfaaf3519ee59
victor <hugohg34@gmail.com>
2021-10-17 21:14:48 +0200

    WinnerRuleCord implementado

--- src/main/java/controller/utils/WinnerRuleCord.java
+++ src/main/java/controller/utils/WinnerRuleCord.java
@@ -6,9 +6,10 @@ import java.util.List;
 import model.Board;
 import model.DiscDrop;
 import types.DiscColor;
+import utils.CardinalDirection;
 import utils.Coordinate;
 
-public abstract class WinnerRuleCord {
+public class WinnerRuleCord {
 
 	protected Board board;
 	protected int goal = 4;
@@ -17,22 +18,19 @@ public abstract class WinnerRuleCord {
 		this.board = board;
 	}
 
-	public boolean check(DiscDrop discDrop) {
-		Coordinate discCoordinate = new Coordinate(discDrop.getColumn(), discDrop.getRow());
-		List<Coordinate> neighbordCoords = getDisplacedList(discCoordinate, goal);
-		List<DiscColor> neighbordDiscDrop = getNeighbordDiscDrop(neighbordCoords);
-
-	}
-
-	public List<Coordinate> getDisplacedList(Coordinate coordinate, int numDisplaced) {
-		List<Coordinate> neighbordList = new ArrayList<>();
-		for (int i = 1; i < numDisplaced; i++) {
-			neighbordList.add(getDisplaced(coordinate, i));
+	public boolean isFourInLine(DiscDrop discDrop) {
+		for (CardinalDirection direction : CardinalDirection.values()) {
+			List<DiscColor> neighbordDiscDrop = getNeighbordDiscDrop(discDrop, direction);
+			if (neighbordDiscDrop.size() == this.goal && isEqualColor(discDrop, neighbordDiscDrop)) {
+				return true;
+			}
 		}
-		return neighbordList;
+		return false;
 	}
 
-	public List<DiscColor> getNeighbordDiscDrop(List<Coordinate> coordinateList) {
+	public List<DiscColor> getNeighbordDiscDrop(DiscDrop discDrop, CardinalDirection direction) {
+		Coordinate discCoordinate = new Coordinate(discDrop.getColumn(), discDrop.getRow());
+		Coordinate[] coordinateList = discCoordinate.getNeighbords(direction, goal - 1);
 		List<DiscColor> neighbordList = new ArrayList<>();
 		for (Coordinate coord : coordinateList) {
 			if (inBoardCuadrant(coord)) {
@@ -42,47 +40,13 @@ public abstract class WinnerRuleCord {
 		return neighbordList;
 	}
 
-	protected abstract Coordinate getDisplaced(Coordinate coortinate, int displacement);
-
-	///////////////
-	public boolean isFourInLine() {
-		boolean isInline = false;
-		for (int column = 1; column < Board.COLUMNS && !isInline; column++) {
-			for (int row = 1; row < Board.ROWS && !isInline; row++) {
-				isInline = check(column, row);
+	private boolean isEqualColor(DiscDrop discDrop, List<DiscColor> neighbordDiscDrop) {
+		for (DiscColor currentDisc : neighbordDiscDrop) {
+			if (currentDisc != discDrop.getColor()) {
+				return false;
 			}
 		}
-		return isInline;
-	}
-
-	protected abstract Coordinate getDisplaced(Coordinate coortinate);
-
-	private boolean check(int column, int row) {
-		List<DiscColor> neighborsList = neighborsList(column, row);
-		int inLine = 1;
-		DiscColor currentDisc = neighborsList.get(0);
-		for (int i = 1; i < neighborsList.size(); i++) {
-			if (currentDisc != DiscColor.NULL && currentDisc == neighborsList.get(i)) {
-				inLine++;
-			} else {
-				inLine = 1;
-			}
-			currentDisc = neighborsList.get(i);
-		}
-		return inLine == this.goal;
-	}
-
-	private List<DiscColor> neighborsList(int column, int row) {
-		List<DiscColor> discList = new ArrayList<>();
-		discList.add(this.board.getDisc(column, row));
-		Coordinate displacedCoord = new Coordinate(column, row);
-		for (int i = 1; i < this.goal; i++) {
-			displacedCoord = getDisplaced(displacedCoord);
-			if (inBoardCuadrant(displacedCoord)) {
-				discList.add(this.board.getDisc(displacedCoord.getX(), displacedCoord.getY()));
-			}
-		}
-		return discList;
+		return true;
 	}
 
 	private boolean inBoardCuadrant(Coordinate coordinate) {
@@ -90,4 +54,5 @@ public abstract class WinnerRuleCord {
 		Coordinate rowBoardCord = new Coordinate(1, Board.ROWS);
 		return columBoardCord.inCuadrant(coordinate) && rowBoardCord.inCuadrant(coordinate);
 	}
+
 }
