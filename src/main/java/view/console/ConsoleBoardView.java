90cd0d3f8045cf7feed084aa1bc8e366b2931f95
victor <hugohg34@gmail.com>
2021-10-18 01:44:30 +0200

    Formato de boardVew

--- src/main/java/view/console/ConsoleBoardView.java
+++ src/main/java/view/console/ConsoleBoardView.java
@@ -7,23 +7,38 @@ import view.BoardView;
 public class ConsoleBoardView implements BoardView {
 
 	private Board board;
+	private Console console = Console.getInstance();
+	private String cellFormat = "| %s |";
+	String separator = String.format("|%33s|", "").replace(" ", "-");
 
 	@Override
 	public void show() {
-		assert (this.board != null);
-		Console console = Console.getInstance();
-		String separator = String.format("|%33s|", "").replace(" ", "-");
-		console.println(separator);
-		console.println(String.format("|%14s%s%14s|", "", "BOARD", ""));
-		console.println(separator);
+		assert (this.board != null);		
+		printHeader();
 		for (int row = Board.ROWS; row > 0; row--) {
 			for (int column = 1; column <= Board.COLUMNS; column++) {
 				char discChar = this.board.getDisc(column, row).getChar();
-				console.print(String.format("| %s |", discChar));
+				this.console.print(String.format(cellFormat, discChar));
 			}
-			Console.getInstance().println("");
+			console.println("");
 		}
+		printFooter(separator);
+		
+	}
+
+	private void printFooter(String separator) {
 		console.println(separator);
+		for (int column = 1; column <= Board.COLUMNS; column++) {
+			this.console.print(String.format(cellFormat, column));
+		}
+		console.println("");
+		console.println(separator);
+	}
+
+	private void printHeader() {
+		this.console.println(separator);
+		this.console.println(String.format("|%14s%s%14s|", "", "BOARD", ""));
+		this.console.println(separator);
 	}
 
 	@Override
