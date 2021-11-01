7f2811b1f951515a9dc9722f5220b1e9e2a1c443
victor <hugohg34@gmail.com>
2021-11-01 12:37:40 +0100

    add turn test

--- src/test/java/usantatecla/connect4/models/TurnTest.java
+++ src/test/java/usantatecla/connect4/models/TurnTest.java
@@ -2,13 +2,32 @@ package usantatecla.connect4.models;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import usantatecla.utils.models.Color;
 
 class TurnTest {
 
+	private Turn turn;
+
+	@BeforeEach
+	void beforeEach() {
+		this.turn = new Turn(new Board());
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void givenCurrentPlayer_whenNextTurn_thenPlayerColorNotEquals() {
+		Color currentColor = this.turn.getActiveColor();
+		this.turn.next();
+		assertNotEquals(currentColor, this.turn.getActiveColor());
+	}
+
+	@Test
+	void givenCurrentPlayer_whenNextTurn_thenPlayerNotEqual() {
+		Player currentPlayer = this.turn.getActivePlayer();
+		this.turn.next();
+		assertNotEquals(currentPlayer, this.turn.getActivePlayer());
 	}
 
 }
