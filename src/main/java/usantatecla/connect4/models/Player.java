fb5ca53725dff0f371bd821522935f1df0699f54
victor <hugohg34@gmail.com>
2021-11-06 17:24:00 +0100

    Eliminando cod muerto

--- src/main/java/usantatecla/connect4/models/Player.java
+++ src/main/java/usantatecla/connect4/models/Player.java
@@ -6,7 +6,6 @@ class Player {
 
 	private Color color;
 	private Board board;
-	private int putTokens;
 
 	Player(Color color, Board board) {
 		assert !color.isNull();
@@ -14,12 +13,10 @@ class Player {
 
 		this.color = color;
 		this.board = board;
-		this.putTokens = 0;
 	}
 
 	void putToken(int column) {
 		this.board.putToken(column, this.color);
-		this.putTokens++;
 	}
 
 	Error getPutTokenError(int column) {
@@ -31,10 +28,6 @@ class Player {
 
 	Color getColor() {
 		return this.color;
-	}
-
-	public void set(int putTokens) {
-		this.putTokens = putTokens;
 	}
 
 }
